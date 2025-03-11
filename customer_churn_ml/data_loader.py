173eb5a4ee2fa8db144440b5d8d2fa26c56881fd
Nishan-k <karki.nishann@gmail.com>
2025-03-11 22:58:49 +0100

    data_loader.py is ready to go

---
+++ customer_churn_ml/data_loader.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from db import execute_query
+
+
+def load_data():
+    """
+    Load all the data from the customers table in a dataframe:
+    """
+
+    query = "SELECT * FROM customer;"
+    result = execute_query(query=query)
+
+    if result:
+        df = pd.DataFrame(result)
+        return df
+    else:
+        return None
+    
+
+
+test = load_data()
+print(test.head(2))
--- customer_churn_ml/db.py
+++ customer_churn_ml/db.py
@@ -2,3 +2,28 @@ import psycopg2
 from psycopg2 import sql
 from credentials import db_config
 
+
+
+def execute_query(query):
+    """"
+    Creates DB connection, executes the query, and returns the result.
+    """
+
+    try:
+        connection = psycopg2.connect(**db_config)
+        cursor = connection.cursor()
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        return result
+    
+    except psycopg2.Error as e:
+        print(f"An error has occured: {e}")
+        return None
+    
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
