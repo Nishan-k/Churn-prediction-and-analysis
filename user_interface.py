66a490b14793bb3c0630211facdb41e99ebf3dfd
Nishan-k <karki.nishann@gmail.com>
2025-04-06 20:45:47 +0200

    Sessions handled

--- user_interface.py
+++ user_interface.py
@@ -284,84 +284,8 @@ if page == "📊 Predict":
             res = requests.post(url="http://127.0.0.1:8000/predict", json=input_features)
             if res.status_code == 200:
                 customer_health_dashboard(res, input_features=input_features)
-            
-            # Persistent display (works after page navigation)
-            # if st.session_state.show_dashboard:
-            #     show_customer_dashboard()  # Session state version
-                
-            #     if st.button("Clear Dashboard"):
-            #         st.session_state.show_dashboard = False
-            #         st.session_state.shap_plot = None
-            #         st.rerun()
-                
-            
-            # prediction = res.json()['Prediction']
-            # st.session_state.prediction_result = prediction
-            # prediction_prob = res.json()['Prediction_proba'] * 100
-            # delta_precentage = abs(baseline_churn_rate  - prediction_prob)
+                     
 
-
-            # st.write("")
-            # st.subheader("Customer Health Dashboard")
-            
-            # m1, m2, m3 = st.columns(3)
-
-            # # Delta percentage:
-            # m1.metric("Churn Risk", 
-            #           "🟢 Low" if prediction == 0 else "🔴 High",
-            #           delta=f"{delta_precentage:.2f}% better than average" if prediction == 0 else f"{delta_precentage:.2f}% worse than average")
-            
-            # # Prediction Probability:
-            # m2.metric(label= "Prediction Confidence", value=f"{prediction_prob:.2f}%", delta="Model's confidence")
-
-            # # Customer Life-Time Value:
-            # contract_length = contract_mapping.get(contract)
-            # expected_remaining_tenure = max(contract_length - tenure, 0)
-            # ltv = monthly_charges * expected_remaining_tenure
-
-            # m3.metric("Customer Life Time Value", f"{ltv:.2f} €", delta="Expected Amount")
-
-            # # Risk visualization
-            # risk_level = 100 - prediction_prob if prediction == 0 else prediction_prob
-            # st.write("Risk Level")
-            # st.progress(int(risk_level))
-
-            # st.write("")
-            
-            
-            # customer_data = pd.DataFrame.from_dict({k: [v] for k, v in input_features.items()})
-            # result = create_clean_shap_dashboard(customer_data=customer_data)
-            # shap_values = result["shap_values"]
-            # st.session_state.customer_data = customer_data
-            # st.session_state.shap_values = shap_values
-
-            # st.subheader("Prediction Result")
-            # prediction = result["prediction"]
-            # probability = result["churn_probability"] * 100
-    
-            # # Display prediction with formatting
-            # if prediction == "Churn":
-            #     st.error(f"Customer is predicted to churn with {probability:.1f}% probability")
-            # else:
-            #     st.success(f"Customer is predicted to stay with {(100-probability):.1f}% probability")
-            
-            # # Display the plot
-            # st.subheader("Feature Impact Analysis")
-            # st.pyplot(result["plot"])
-            # st.write("")
-            # st.write("")
-            # st.info("👉Now, you can go to '📖 Explain' page or '💡 Recommendations' or 📑 Generate Report page for further actions for this customer in the Navigation bar.")
-            # st.write("")
-
-
-                
-
-
-
-        
-   
-
-            
 
 
 if page == "📖 Explain":
@@ -376,13 +300,27 @@ if page == "📖 Explain":
         
 
 if page == "💡 Recommendations":
-     st.dataframe(st.session_state.customer_data)
+     st.title("Recommendations")
+     if st.session_state.customer_data is None:
+        st.warning("Please make a prediction first!")
+        if st.button("Go to Prediction Page"):
+            st.session_state.page_selection = "📊 Predict" 
+            st.rerun()
+     else:
+        st.dataframe(st.session_state.customer_data)
 
 
 if page == "📑 Generate Report":
-    st.write(st.session_state.shap_values)
-    st.write("")
-    st.write(st.session_state.prediction_result)
+    st.title("Generate a report for the stakeholder using LLMs")
+    if st.session_state.customer_data is None:
+        st.warning("Please make a prediction first!")
+        if st.button("Go to Prediction Page"):
+            st.session_state.page_selection = "📊 Predict" 
+            st.rerun()
+    else:
+        st.write(st.session_state.shap_values)
+        st.write("")
+        st.write(st.session_state.prediction_result)
 
 ################################
 ### ABOUT PAGE:
